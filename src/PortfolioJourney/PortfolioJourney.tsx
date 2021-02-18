8ab0b1b821e6951758621dadce18b4c07ad788e3
Alessio Fanelli <fana@alessiofanelli.com>
2021-02-18 15:58:55 -0500

    Add alternating directions

--- src/PortfolioJourney/PortfolioJourney.tsx
+++ src/PortfolioJourney/PortfolioJourney.tsx
@@ -3,7 +3,7 @@ import StarDestroyerBridge from './StarDestroyerBridge';
 import Stars from './Stars';
 import PortfolioLogo from './PortfolioLogo';
 
-const LOGO_NAMES = ['oort', 'panther']
+const LOGO_NAMES = ['oort', 'goldbelly']
 
 export default function PortfolioJourney() {
   return (
@@ -11,8 +11,8 @@ export default function PortfolioJourney() {
       <Stars />
       {LOGO_NAMES.map((companyName, index) => {
         return (
-          <Sequence from={100 + (index * 1000)} durationInFrames={200}>
-            <PortfolioLogo name={companyName} />
+          <Sequence from={100 + (index * 200)} durationInFrames={300}>
+            <PortfolioLogo name={companyName} itemNumber={index} />
           </Sequence>
         )
       })}
--- src/PortfolioJourney/PortfolioLogo.tsx
+++ src/PortfolioJourney/PortfolioLogo.tsx
@@ -2,13 +2,22 @@ import { Img, interpolate, useCurrentFrame, useVideoConfig } from 'remotion';
 
 interface logoProps {
   name: string;
+  itemNumber: number;
 }
 
-export default function PortfolioLogo({name}:logoProps) {
+export default function PortfolioLogo({name, itemNumber}:logoProps) {
   const frame = useCurrentFrame();
-  const { width, height, durationInFrames } = useVideoConfig();
-  const left = interpolate(frame, [0, 200], [width * 0.9, 0]);
-  const top = interpolate(frame, [0, 200], [0, height]);
+  const { width, height, durationInFrames } = useVideoConfig(); 
+  let left, top = 0;
+
+  if (itemNumber % 2) {
+    left = interpolate(frame, [150, 300], [width * 0.6, 0]);
+    top = interpolate(frame, [50, 300], [0, height]);
+  } else {
+    left = interpolate(frame, [width * 0.6, 0], [150, 300]);
+    top = interpolate(frame, [50, 300], [0, height]);
+  }
+
   const logoWidth = interpolate(frame, [0, 200], [50, 300]);
   const logo = require(`./images/${name}.png`)
 
